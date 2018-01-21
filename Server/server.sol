af839968876607bb6a664902e9e8dc3526a260d0
Catherine <lili@Cats-Mac.fios-router.home>
2018-01-21 08:13:47 -0500

    server update

--- Server/server.sol
+++ Server/server.sol
@@ -15,27 +15,33 @@ contract DB {
         bool  offered;
         uint  amount_return;
         address lender;
+        address borrower;
+        bool accepted;
+        bool returned;
     }
     
     mapping (address => UserInfo) public userinfo;
     Request[] public pool;
 
-    function myscore() public returns (uint){
-        UserInfo myinfo = userinfo[msg.sender];
+    function myscore() public view returns (uint){
+        UserInfo storage myinfo = userinfo[msg.sender];
         return myinfo.req_history.length;
     }
     
-    function submit_request(uint amount, uint expire){
+    function submit_request(uint amount, uint expire) public{
         Request memory req;
         req.amount = amount;
         req.expire_ts = expire;
-        pool.push(req);
-        Request storage new_req = pool[pool.length - 1];
-        userinfo[msg.sender].req_history.push(new_req);
+        req.borrower = msg.sender;
+        userinfo[msg.sender].req_history.push(req);
+        Request storage new_req = userinfo[msg.sender].req_history
+        [userinfo[msg.sender].req_history.length - 1];
+        pool.push(new_req);
     }
     
 
-    function submit_offer(uint order_idx, uint amount) payable returns (bool){
+    function submit_offer(uint order_idx, uint amount) payable public 
+    returns (bool){
 
         bool success;
         
@@ -49,7 +55,7 @@ contract DB {
                 success = true;
                 req.offered = true;
                 req.amount_return = amount;
-                req.lender.send(req.amount);
+                req.lender.transfer(req.amount);
                 req.lender = msg.sender;
             }
             else{
@@ -58,11 +64,37 @@ contract DB {
         }
         
         if(!success){
-            msg.sender.send(msg.value);
+            msg.sender.transfer(msg.value);
         }
         
         return success;
     }
+    
+    function remove_from_pool(uint order_idx) {
+        uint last_idx = pool.length - 1;
+        if(last_idx >= 0){
+            pool[order_idx] = pool[last_idx];
+            delete pool[last_idx];
+            pool.length --;
+        }
+    }
+    
+    function accept_offer(uint order_idx) public returns (bool){
+        if (order_idx >= pool.length){
+            return false;
+        }
+        Request storage req = pool[order_idx];
+        if (req.borrower != msg.sender || req.accepted || (!req.offered)){
+            return false;
+        }
+        
+        msg.sender.transfer(req.amount);
+        
+        remove_from_pool(order_idx);
+        
+        return true;
+    }
+    
     
 }
 
